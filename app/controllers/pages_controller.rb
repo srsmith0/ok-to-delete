293eab40be77ea3b6560bbb6c7bd190df2fedd10
Shawn Smith <srsmith11@live.com>
2020-05-18 14:36:52 -0600

    added edit/update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,22 @@ class PagesController < ApplicationController
     # all these methods go to a folder views/pages/new.html/txt/etc..
   end
 
+  def edit
+    #want to give the form to edit with all information on it
+    #pass the ID to identify it.. use params
+    @page = Page.find(params[:id]) #id is passed through URL
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if (@page.update(pages_params))
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @page = Page.new(pages_params)
     if @page.save
