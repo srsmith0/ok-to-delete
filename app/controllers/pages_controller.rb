3680b21bbb3f1c64e4239f3a0b3c8186370e24c6
Shawn Smith <srsmith11@live.com>
2020-05-18 14:16:08 -0600

    Added new page functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,33 @@
 class PagesController < ApplicationController
   def index
+    @pages = Page.all #controller is interacting with model here
+
+    #although we don't do anything, rails automatically does this for us
+    
   end
 
   def show
+  #find a page but we need an ID
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
+    # all these methods go to a folder views/pages/new.html/txt/etc..
   end
+
+  def create
+    @page = Page.new(pages_params)
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 end
